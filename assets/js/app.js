95092cb234110a04da250f59b31dcc8dfed26500
sdesousa <sylvaindesousa@free.fr>
2019-11-30 09:23:58 +0100

    css

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,11 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
+const imagesContext = require.context('../images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesContext.keys().forEach(imagesContext);
